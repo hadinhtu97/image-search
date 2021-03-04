504d2c6811e9da805967a81efb209732ff831cb9
Tu Ha Dinh <hadinhtu97@gmail.com>
2021-03-04 09:48:37 +0700

    Styling index page

--- pages/index.js
+++ pages/index.js
@@ -34,60 +34,63 @@ const Home = () => {
                 <title>Image Search</title>
             </Head>
             <main className={styles.main}>
-                <div className={styles.alert}>
-                    as the developer of this website, I will not be responsible for the structure of the frontend being broken when you choose size xxlarge or huge
+
+                <div className={styles.form} style={query == '' ? { height: '20rem' } : {}}>
+                    <form>
+                        <input className={styles.query} type='search' placeholder='funny cat' value={query} onChange={e => setQuery(e.target.value)} />
+                        <select className={styles.select} value={num} onChange={e => setNum(e.target.value)}>
+                            <option value={1}>1</option>
+                            <option value={2}>2</option>
+                            <option value={3}>3</option>
+                            <option value={4}>4</option>
+                            <option value={5}>5</option>
+                            <option value={6}>6</option>
+                            <option value={7}>7</option>
+                            <option value={8}>8</option>
+                            <option value={9}>9</option>
+                            <option value={10}>10</option>
+                        </select>
+                        <select className={styles.select} value={page} onChange={e => setPage(e.target.value)}>
+                            <option value={1}>1</option>
+                            <option value={2}>2</option>
+                            <option value={3}>3</option>
+                            <option value={4}>4</option>
+                            <option value={5}>5</option>
+                            <option value={6}>6</option>
+                            <option value={7}>7</option>
+                            <option value={8}>8</option>
+                            <option value={9}>9</option>
+                        </select>
+                        <select className={styles.select} value={size} onChange={e => setSize(e.target.value)}>
+                            <option value='icon'>icon</option>
+                            <option value='small'>small</option>
+                            <option value='medium'>medium</option>
+                            <option value='large'>large</option>
+                            <option value='xlarge'>xlarge</option>
+                            <option value='xxlarge'>xxlarge</option>
+                            <option value='huge'>huge</option>
+                        </select>
+                        <select className={styles.select} value={color} onChange={e => setColor(e.target.value)}>
+                            <option value='color'>color</option>
+                            <option value='gray'>gray</option>
+                            <option value='mono'>mono</option>
+                            <option value='trans'>trans</option>
+                        </select>
+                        <select className={styles.select} value={type} onChange={e => setType(e.target.value)}>
+                            <option value='jpg'>jpg</option>
+                            <option value='png'>png</option>
+                        </select>
+                        <input className={styles.submit} type='submit' value='search' onClick={getImagesThenSetImages} /> <br />
+                        <div className={styles.alert}>
+                            as the developer of this website, I will not be responsible if frontend being broken when you choose huge size
+                        </div>
+                    </form>
                 </div>
-                <form>
-                    <input type='text' required placeholder='funny cat' value={query} onChange={e => setQuery(e.target.value)} />
-                    <select value={num} onChange={e => setNum(e.target.value)}>
-                        <option value={1}>1</option>
-                        <option value={2}>2</option>
-                        <option value={3}>3</option>
-                        <option value={4}>4</option>
-                        <option value={5}>5</option>
-                        <option value={6}>6</option>
-                        <option value={7}>7</option>
-                        <option value={8}>8</option>
-                        <option value={9}>9</option>
-                        <option value={10}>10</option>
-                    </select>
-                    <select value={page} onChange={e => setPage(e.target.value)}>
-                        <option value={1}>1</option>
-                        <option value={2}>2</option>
-                        <option value={3}>3</option>
-                        <option value={4}>4</option>
-                        <option value={5}>5</option>
-                        <option value={6}>6</option>
-                        <option value={7}>7</option>
-                        <option value={8}>8</option>
-                        <option value={9}>9</option>
-                    </select>
-                    <select value={size} onChange={e => setSize(e.target.value)}>
-                        <option value='icon'>icon</option>
-                        <option value='small'>small</option>
-                        <option value='medium'>medium</option>
-                        <option value='large'>large</option>
-                        <option value='xlarge'>xlarge</option>
-                        <option value='xxlarge'>xxlarge</option>
-                        <option value='huge'>huge</option>
-                    </select>
-                    <select value={color} onChange={e => setColor(e.target.value)}>
-                        <option value='color'>color</option>
-                        <option value='gray'>gray</option>
-                        <option value='mono'>mono</option>
-                        <option value='trans'>trans</option>
-                    </select>
-                    <select value={type} onChange={e => setType(e.target.value)}>
-                        <option value='jpg'>jpg</option>
-                        <option value='png'>png</option>
-                    </select>
-                    <input type='submit' value='search' onClick={getImagesThenSetImages} />
-                </form>
-                <div>
+                <div className={styles.images}>
                     {
                         images == null ? <></> : images.map((image, i) =>
                             <a key={i} href={image.pageURL} target='_blank' title={image.description}>
-                                <img src={image.url} width={image.width} height={image.height} />
+                                <img className={styles.image} src={image.url} width={image.width} height={image.height} />
                             </a>
                         )
                     }
